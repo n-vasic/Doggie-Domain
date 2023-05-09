3c33f1439432e4a82ec4137c7543eb763d8bec6e
n-vasic <nvasic66@yahoo.com>
2023-05-09 16:02:11 +0200

    created jwt token and login controller
    installed jsonwebtoken, and created loginController which handles the login and tested login with postman tool

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
-import { hashPassword } from '../helpers/authHelper.js';
+import { comparePassword, hashPassword } from '../helpers/authHelper.js';
 import userModel from '../models/userModel.js';
-
+import JWT from 'jsonwebtoken';
 export const registerController = async (req, res) => {
   try {
     const { name, email, password, phone, address } = req.body;
@@ -43,15 +43,66 @@ export const registerController = async (req, res) => {
       password: hashedPassword,
     }).save();
     res.status(200).send({
-        success:true,
-        message:'User registered successfully',
-        user
-    })
+      success: true,
+      message: 'User registered successfully',
+      user,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
       mesagge: 'Error While Registering',
+      error,
+    });
+  }
+};
+
+//LOGIN || POST
+
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //validations
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: 'Invalid email or password',
+      });
+    }
+    //check user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: 'Account with that email is not registered',
+      });
+    }
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res
+        .status(200)
+        .send({ success: false, message: 'Invalid Password' });
+    }
+    //token
+    const token = JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+    });
+    res.status(200).send({
+        success:true,
+        message:'Login succesful',
+        user:{
+            name:user.name,
+            email:user.email,
+            phone:user.phone,
+            address:user.address
+        },
+        token
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: 'Error while loging in',
       error,
     });
   }
